47d785881bdd512d8930e5af3f1433d3c2fbbacd
Anton Lookin <anton.lookin@gmail.com>
2015-12-21 22:50:16 +0200

    AutoLayout constraint for scroll view content

--- task2/task2/task2/ViewController.m
+++ task2/task2/task2/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *textLabel;
+
 @end
 
 @implementation ViewController
@@ -17,6 +19,31 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	
+	
+}
+
+- (IBAction) addButtonTapped:(id)sender {
+	NSString* stringTemplate = @"Random styled paragraph skjdf jksfjksfksjkf sldkfj klsjfkl sjlfjskldjfkl sjlfjs lkdfjkls jdfskj";
+	NSMutableAttributedString *paragraphContent = [[NSMutableAttributedString alloc]
+												   initWithString:stringTemplate
+												   attributes:@{
+																NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue" size:arc4random_uniform(20)],
+																NSForegroundColorAttributeName: [UIColor grayColor]
+																}];
+	NSMutableParagraphStyle *paragraph = [[NSMutableParagraphStyle alloc] init];
+	paragraph.alignment = NSTextAlignmentRight;
+	paragraph.lineSpacing = 10;
+	paragraph.lineBreakMode = NSLineBreakByCharWrapping;
+	
+	[paragraphContent addAttribute:NSParagraphStyleAttributeName value:paragraph range:NSMakeRange(0, 1)];
+	NSAttributedString *lineBreak = [[NSAttributedString alloc] initWithString:@"\n" attributes:nil];
+	[paragraphContent insertAttributedString:lineBreak atIndex:0];
+	
+	NSMutableAttributedString *labelContent = [[NSMutableAttributedString alloc] initWithAttributedString:self.textLabel.attributedText];
+	[labelContent appendAttributedString:paragraphContent];
+	
+	self.textLabel.attributedText = labelContent;
 }
 
 - (void)didReceiveMemoryWarning {
